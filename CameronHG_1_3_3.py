164a475357b38abe843437004ca02063cc576210
Cameron <57145246+CameronHG@users.noreply.github.com>
2020-01-28 10:45:08 -0600

    Update CameronHG_1_3_3.py

--- CameronHG_1_3_3.py
+++ CameronHG_1_3_3.py
@@ -5,3 +5,42 @@ a**2
 a==3
 
 a+1 >= 2 and a**2 != 5
+
+a**2 >= 9 and not a>3
+
+a+2 == 5 or a-1 != 3
+
+# Prediction True
+
+a**2 >= 9 and not a>3
+
+# Prediction True
+
+a+2 == 5 or a-1 != 3
+
+
+
+def age_limit_output(age) :
+
+    AGE_LIMIT = 13
+    if age < AGE_LIMIT:
+        print(age, 'is below the age limit.')
+    else:
+        print(age, 'is old enough.')
+    print(' minimum age is ', AGE_LIMIT)
+
+age_limit_output(10)
+age_limit_output(16)
+
+def report_grade(percent) :
+
+    GRADE_CUTOFF = 80
+    if age >= GRADE_CUTOFF:
+        print('A grade of ', percent, ' indicates mastery.' )
+        print(' Keep up the good work!')
+    else:
+        print('A grade of ', percent, ' doesnt indicate mastery.' )
+        print(' Youre garbage. Absolute trash.')
+
+report_grade(70)
+report_grade(80)
